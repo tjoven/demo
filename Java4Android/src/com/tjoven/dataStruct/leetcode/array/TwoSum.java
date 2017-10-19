f7131c01de1b9283119df9f4dad59e8658fc1b94
tjoven <290707474@qq.com>
2017-10-19 22:01:10 +0800

    twosum2

--- Java4Android/src/com/tjoven/dataStruct/leetcode/array/TwoSum.java
+++ Java4Android/src/com/tjoven/dataStruct/leetcode/array/TwoSum.java
@@ -5,17 +5,39 @@ import java.util.Map;
 
 public class TwoSum {
 
-	static int[] arrays = {1,2,3,5};
+	static int[] arrays = {2,3,4};
 	public static void main(String[] args) {
-		int[] re = twoSum(arrays, 4);
+		int[] re = twoSum(arrays,6);
+		if(re == null){
+			System.out.println("Null");
+			return;
+		}
 		for (int i : re) {
 			System.out.println(i);
 		}
 	}
+	
+	public static int[] twoSum2(int[] nums, int target) {
+        Map map = new HashMap<Integer,Integer>(nums.length);
+        for(int i=0;i<nums.length;i++){
+        	boolean b = map.containsKey(target - nums[i]);
+            if(b){
+         	   int[] as = new int[2];
+         	   as[0] = (Integer) map.get(target - nums[i]);
+         	   as[1] = i;
+         	   return as;
+         	   
+            }
+            map.put(nums[i], i);
+        }
+        
+        return null;
+    }
+	
 	public static int[] twoSum(int[] nums, int target) {
         Map map = new HashMap<Integer,Integer>(nums.length);
         for(int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
+            map.put(target-nums[i], i);
         }
         
         for(int j =0;j<nums.length;j++){
@@ -24,7 +46,9 @@ public class TwoSum {
         	   int[] as = new int[2];
         	   as[0] = (Integer) map.get(nums[j]);
         	   as[1] = j;
-        	   return as;
+        	   if(as[0]!=as[1]){
+        		   return as;
+        	   }
            }
         }
         return null;
