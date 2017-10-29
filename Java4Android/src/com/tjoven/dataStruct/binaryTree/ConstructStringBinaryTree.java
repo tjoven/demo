26f39547f7e6ac731b4a666a460bb4a320aad1ac
tjoven <290707474@qq.com>
2017-10-29 16:53:44 +0800

    treetostr by stack

--- Java4Android/src/com/tjoven/dataStruct/binaryTree/ConstructStringBinaryTree.java
+++ Java4Android/src/com/tjoven/dataStruct/binaryTree/ConstructStringBinaryTree.java
@@ -1,12 +1,63 @@
 package com.tjoven.dataStruct.binaryTree;
 
+import java.util.Stack;
+
 public class ConstructStringBinaryTree {
 
 	public static void main(String[] args) {
-		String restult = tree2str(null);
+		String restult = tree2str2(TreeNodeBuild.get());
 		System.out.println(restult);
+		
 	}
-    public static String tree2str(TreeNode t) {
+	/**
+	 * 自己构建stack实现
+	 * @param t
+	 * @return
+	 */
+	private static String tree2str2(TreeNode t){
+		if(t==null){
+			return "";
+		}
+		StringBuffer buffer = new StringBuffer();
+		Stack<Object> stack = new Stack<Object>();
+		stack.push(t);
+		
+		while(!stack.isEmpty()){
+			Object object = stack.pop();
+			if(object instanceof TreeNode){
+				TreeNode node = (TreeNode)object;
+				buffer.append(node.val+"");
+				if(node.right==null && node.left == null){
+					continue;
+				}
+				if(node.right!=null){
+					stack.push(")");
+					stack.push(node.right);
+					stack.push("(");
+				}
+				if(node.left!=null){
+					stack.push(")");
+					stack.push(node.left);
+					stack.push("(");
+				}else if(node.left == null){
+					stack.push(")");
+					stack.push("(");
+				}
+			}else if(object instanceof String){
+				buffer.append(object);
+			}
+			
+		}
+		return buffer.toString();
+		
+	}
+	
+	/**
+	 * 递归实现
+	 * @param t
+	 * @return
+	 */
+	public static String tree2str(TreeNode t) {
         if(t==null){
             return "";
         }
