6eba36ab86b4bc1621337b9fa23e8b94b57e6f92
zhaowen.tian <zhaowen.tian@ucarinc.com>
2018-05-04 14:26:34 +0800

    RotateArray

---
+++ Java4Android/src/com/tjoven/dataStruct/leetcode/array/RotateArray.java
@@ -0,0 +1,37 @@
+package com.tjoven.dataStruct.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode.com/problems/rotate-array/description/
+ * @author car
+ *
+ */
+public class RotateArray {
+
+	static int[] nums = {1,2,3,4,5,6};
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		rotate02(nums, 5);
+		System.out.println(Arrays.toString(nums));
+	}
+
+	public static void rotate(int[] nums, int k) {
+        for (int i = 0; i <k; i++) {
+        	int last = nums[nums.length-1];
+        	System.arraycopy(nums, 0, nums, 1,nums.length-1);
+        	nums[0] = last;
+		}
+    }
+	
+	public static void rotate02(int[] nums, int k) {
+        int[] result = new int[nums.length];
+        for(int i = 0;i<nums.length;i++){
+        	int newIndex = (i+k)%nums.length;
+        	result[newIndex] = nums[i];
+        }
+        
+        System.out.println(Arrays.toString(result));
+    }
+}
