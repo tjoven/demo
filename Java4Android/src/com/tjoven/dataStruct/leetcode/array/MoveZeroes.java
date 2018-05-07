911ed21381005ea1d0d7f7653d812c2136115133
tjoven <290707474@qq.com>
2018-05-07 22:10:40 +0800

    Move Zeroes

--- Java4Android/src/com/tjoven/dataStruct/leetcode/array/MoveZeroes.java
+++ Java4Android/src/com/tjoven/dataStruct/leetcode/array/MoveZeroes.java
@@ -1,5 +1,7 @@
 package com.tjoven.dataStruct.leetcode.array;
 
+import java.util.Arrays;
+
 /**
  * https://leetcode.com/problems/move-zeroes/description/
  * @author car
@@ -7,9 +9,34 @@ package com.tjoven.dataStruct.leetcode.array;
  */
 public class MoveZeroes {
 
+	static int[] nums = {2, 1, 1, 3, 12};
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		moveZeroes(nums);
+		System.out.print(Arrays.toString(nums));
 	}
+	
+	public static void moveZeroes(int[] nums) {
+		int index = 0;
+		int zeroLen = 0;
+        for (int i = 0;i<nums.length;i++) {
+			if(nums[i] != 0){
+				nums[index] = nums[i];
+				index++;
+			}else{
+				zeroLen++;
+			}
+		}
+        
+        for(int i = nums.length-1;i>=nums.length-zeroLen;i--){
+        	nums[i] = 0;
+        }
+    }
 
+	private void exchange(int a,int b){
+		int temp;
+		temp = a;
+		a= b;
+		b = temp;
+	}
 }
