bcc683ea56e302479656dc736bcaa22bb217d0d8
tjoven <290707474@qq.com>
2018-05-16 23:41:10 +0800

    题解

--- Java4Android/src/com/tjoven/dataStruct/leetcode/array/MaxmiumSubarray.java
+++ Java4Android/src/com/tjoven/dataStruct/leetcode/array/MaxmiumSubarray.java
@@ -1,5 +1,6 @@
 package com.tjoven.dataStruct.leetcode.array;
 
+
 public class MaxmiumSubarray {
 
 	static int[] nums = {2,-1,-3,4,-1,2,1,-5,4};
@@ -8,6 +9,16 @@ public class MaxmiumSubarray {
 		System.out.println(maxSubArray(nums));
 	}
 	
+	
+	/**
+	 * 题解：
+		这道题要求 求连续的数组值，加和最大。
+ 		试想一下，如果我们从头遍历这个数组。对于数组中的其中一个元素，它只有两个选择：
+ 			1. 要么加入之前的数组加和之中（跟别人一组）
+ 			2. 要么自己单立一个数组（自己单开一组）
+	 * @param nums
+	 * @return
+	 */
 	public static int maxSubArray(int[] nums){
           //    Solution 1:  O(n)
           // check param validation.
